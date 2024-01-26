fca2ec867c13aad688fe1c14169391baabf79cab
raghavProactifi <raghav@proactifi.com>
2024-01-26 14:29:47 -0500

    Created tentative teardown script, script to verify tables were correctly populated

---
+++ dbScripts/verify_tables_exist.sql
@@ -0,0 +1,12 @@
+SHOW TABLES;
+
+DESCRIBE User;
+DESCRIBE Post;
+DESCRIBE Category;
+DESCRIBE PostCategory;
+DESCRIBE TechStack;
+DESCRIBE UserTechStack;
+DESCRIBE Comment;
+DESCRIBE CommentTag;
+DESCRIBE CommentUpvote;
+DESCRIBE PostUpvote;
