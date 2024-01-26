95b888f9b5061fd27dd5ac253aa08167ab3122f0
raghavProactifi <raghav@proactifi.com>
2024-01-26 17:01:30 -0500

    Adjusted schema to add vendor tagging to posts

--- dbScripts/initDb.sql
+++ dbScripts/initDb.sql
@@ -1,76 +1,106 @@
 CREATE TABLE User (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    userName VARCHAR(255) UNIQUE,
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255)
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255),
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)
 );
 
 CREATE TABLE Post (
-    post_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
-    body TEXT,
+    title VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
     creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 CREATE TABLE Category (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
-    category_name VARCHAR(255) UNIQUE
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255) UNIQUE NOT NULL,
+    decription TINYTEXT,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)
 );
 
 CREATE TABLE PostCategory (
     post_id INT,
     category_id INT,
-    FOREIGN KEY (post_id) REFERENCES Post(post_id),
-    FOREIGN KEY (category_id) REFERENCES Category(category_id),
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (post_id) REFERENCES Post(id),
+    FOREIGN KEY (category_id) REFERENCES Category(id),
     PRIMARY KEY (post_id, category_id)
 );
 
-CREATE TABLE TechStack (
-    tech_id INT AUTO_INCREMENT PRIMARY KEY,
-    tech_name VARCHAR(255) UNIQUE
+CREATE TABLE Vendor (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tech_name VARCHAR(255) UNIQUE NOT NULL,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)
 );
 
-CREATE TABLE UserTechStack (
+CREATE TABLE UserVendor (
     user_id INT,
-    tech_id INT,
+    vendor_id INT,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
     FOREIGN KEY (user_id) REFERENCES User(id),
-    FOREIGN KEY (tech_id) REFERENCES TechStack(tech_id),
-    PRIMARY KEY (user_id, tech_id)
+    FOREIGN KEY (vendor_id) REFERENCES Vendor(id),
+    PRIMARY KEY (user_id, vendor_id)
+);
+
+CREATE TABLE PostVendor (
+    post_id INT,
+    vendor_id INT,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (post_id) REFERENCES Post(id),
+    FOREIGN KEY (vendor_id) REFERENCES Vendor(id),
+    PRIMARY KEY (post_id, vendor_id)
 );
 
 CREATE TABLE Comment (
-    comment_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT,
     user_id INT,
-    comment_text TEXT,
-    creation_time DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (post_id) REFERENCES Post(post_id),
+    comment_text TEXT NOT NULL,
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (post_id) REFERENCES Post(id),
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 CREATE TABLE CommentTag (
     comment_id INT,
     tagged_user_id INT,
-    FOREIGN KEY (comment_id) REFERENCES Comment(comment_id),
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (comment_id) REFERENCES Comment(id),
     FOREIGN KEY (tagged_user_id) REFERENCES User(id),
     PRIMARY KEY (comment_id, tagged_user_id)
 );
 
 CREATE TABLE PostUpvote (
-    upvote_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     post_id INT,
     user_id INT,
-    FOREIGN KEY (post_id) REFERENCES Post(post_id),
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (post_id) REFERENCES Post(id),
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
 
 CREATE TABLE CommentUpvote (
-    upvote_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     comment_id INT,
     user_id INT,
-    FOREIGN KEY (comment_id) REFERENCES Comment(comment_id),
+    creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
+    FOREIGN KEY (comment_id) REFERENCES Comment(id),
     FOREIGN KEY (user_id) REFERENCES User(id)
 );
