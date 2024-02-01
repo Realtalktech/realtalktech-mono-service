2fa087da25915d0de8f421eb91271cbfddb9a724
raghavProactifi <raghav@proactifi.com>
2024-02-01 00:42:18 -0500

    Fixed wrong value type error

--- dbScripts/initDb.sql
+++ dbScripts/initDb.sql
@@ -51,7 +51,7 @@ CREATE TABLE Vendor (
     id INT AUTO_INCREMENT PRIMARY KEY,
     vendor_name VARCHAR(255) UNIQUE NOT NULL,
     decription TEXT NOT NULL,
-    vendor_url URL,
+    vendor_url TINYTEXT,
     creation_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3),
     update_time DATETIME(3) DEFAULT CURRENT_TIMESTAMP(3)
 );
