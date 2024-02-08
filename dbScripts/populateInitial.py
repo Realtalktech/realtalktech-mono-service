b028f7ef0714c93fbe54d725e5ffce5e57ad1071
raghavProactifi <raghav@proactifi.com>
2024-02-08 00:19:00 -0500

    Nevermind

--- dbScripts/populateInitial.py
+++ dbScripts/populateInitial.py
@@ -6,70 +6,133 @@ DB_USER = 'admin'
 DB_PASSWORD = 'ReallyRealAboutTech123!'
 DB_NAME = 'RealTalkTechDB'
 
-def insert_categories(cursor):
-    category_names = [
-        "AI", "Engineering", "Operations", "Marketing", "Sales", "Customer Success",
-        "Data", "Product", "HR & Talent", "Finance", "Leadership/Exec", "Founder",
-        "Community"
-    ]
+class DataBuilder:
+    def insert_test_users(self, cursor):
+        """Inserts four test users"""
+        names = ["Elon Gates", "Bill Musk", "Mary Barra", "Kamala Clinton"]
+        employers = ["SuperchargedSoftware", "MacroAdvanced", "General Autos", "Capitol Tech"]
+        self.test_user_ids = []
+        for idx, name in enumerate(names):
+            username = name.replace(" ", "").lower()
+            email = username.join("@example.com")
+            current_company = employers[idx]
 
-    # Insert categories
+            cursor.execute(
+                """INSERT INTO User (full_name, username, password, current_company, email) 
+                VALUES (%s, %s, 'password', %s, %s)""", (name, username, current_company, email)
+            )
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            user_id = cursor.fetchone()['LAST_INSERT_ID()']
+            self.test_user_ids.append(user_id)
 
+    def insert_categories(self,cursor):
+        """Insert categories according to Figma Designs"""
+        category_names = [
+            "AI", "Engineering", "Operations", "Marketing", "Sales", "Customer Success",
+            "Data", "Product", "HR & Talent", "Finance", "Leadership/Exec", "Founder",
+            "Community"
+        ]
 
-
-    #  """This will eventually be removed in lieu of calls to upcoming @POST methods."""
-    # # Insert a test user
-    # cursor.execute("INSERT INTO User (first_name, last_name, username, email, password) VALUES ('Test', 'User', 'test_user', 'test@example.com', 'password')")
-    # cursor.execute("SELECT LAST_INSERT_ID()")
-    # user_id = cursor.fetchone()['LAST_INSERT_ID()']
-
-    # # Insert test categories
-    # category_names = ['test1', 'test2', 'test3', 'test4']
-    # category_ids = []
-    # for name in category_names:
-    #     cursor.execute("INSERT INTO Category (category_name) VALUES (%s)", (name,))
-    #     cursor.execute("SELECT LAST_INSERT_ID()")
-    #     category_id = cursor.fetchone()['LAST_INSERT_ID()']
-    #     category_ids.append(category_id)
-        
-    #     if(name == 'test1' or name == 'test2'):
-    #         # Associate user with the category
-    #         cursor.execute("INSERT INTO UserCategory (user_id, category_id) VALUES (%s, %s)", (user_id, category_id))
+        # Insert categories
+        self.category_ids = []
+        for name in category_names:
+            cursor.execute("INSERT INTO Category (category_name) VALUES (%s)", (name,))
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            category_id = cursor.fetchone()['LAST_INSERT_ID()']
+            self.category_ids.append(category_id)
     
-    # # Insert test vendors
-    # vendor_names = ['vendor1', 'vendor2', 'vendor3']
-    # vendor_ids = []
-    # for name in vendor_names:
-    #     cursor.execute("INSERT INTO DiscoverVendor (vendor_name, vendor_type, description) VALUES (%s, 'Experts in API Testing', 'Experts in API Testing')", (name))
-    #     cursor.execute("SELECT LAST_INSERT_ID()")
-    #     vendor_id = cursor.fetchone()['LAST_INSERT_ID()']
-    #     vendor_ids.append(vendor_id)
+    def insert_discover_categories(self,cursor):
+        """Inserting just the sales category, for now"""
+        categories = ["Sales Tools"]
+        descriptions = ["Tools for Sales"]
+        self.vendor_category_ids = []
+        for idx,category in enumerate(categories):
+            cursor.execute(
+                "INSERT INTO DiscoverCategory (category_name, description) VALUES (%s, %s)", 
+                (category, descriptions[idx]))
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            vendor_category_id = cursor.fetchone()['LAST_INSERT_ID()']
+            self.vendor_category_ids.append(vendor_category_id)
+    
+    def insert_test_vendors(self, cursor):
+        """Inserting Salesforce, HubSpot, Zendesk Sell"""
+        self.vendor_names = ['Salesforce', 'HubSpot', 'Zendesk Sell']
+        vendors = {
+            'Salesforce': {
+                'vendor_name': "Salesforce",
+                'vendor_type': "CRM Software",
+                'description': "Salesforce forces sales",
+                'vendor_hq': "Somewhere"
+            },
 
-    # # Insert mock posts in each vendor and category
-    # post_ids = []
-    # postNum = 1
-    # for vendor_id in vendor_ids:
-    #     for category_id in category_ids:
-    #         for i in range(5):  # 5 posts per vendor per category (5 * 3 * 4 = 60 Total Posts)
-    #             cursor.execute("INSERT INTO Post (user_id, title, body) VALUES (%s, 'Mock title %s', 'Mock body %s')", (user_id, postNum, postNum))
-    #             cursor.execute("SELECT LAST_INSERT_ID()")
-    #             post_id = cursor.fetchone()['LAST_INSERT_ID()']
-    #             post_ids.append(post_id)
+            'HubSpot': {
+                'vendor_name': "HubSpot",
+                'vendor_type': "Inbound Marketing and Sales Software",
+                'description': "The spot for all the hubs",
+                'vendor_hq': "Over the Rainbow"
+            },
 
-    #             # Link post to category
-    #             cursor.execute("INSERT INTO PostCategory (post_id, category_id) VALUES (%s, %s)", (post_id, category_id))
+            'Zendesk Sell': {
+                'vendor_name': "Zendesk Sell",
+                'vendor_type': "Sales CRM",
+                'description': "Feng Shui desks for selling",
+                'vendor_hq': "Beyond the Sea"
+            }                        
+        }
+        self.vendor_ids = []
+        for name in self.vendor_names:
+            # Insert vendors into table
+            cursor.execute(
+                """INSERT INTO DiscoverVendor (vendor_name, vendor_type, description, vendor_hq) 
+                VALUES (%s, %s, %s, %s)""", 
+                (vendors[name]['vendor_name'], 
+                 vendors[name]['vendor_type'],
+                 vendors[name]['description'],
+                 vendors[name]['vendor_hq'])
+            )
+            # Link vendors to categories
+            cursor.execute("SELECT LAST_INSERT_ID()")
+            vendor_id = cursor.fetchone()['LAST_INSERT_ID()']
+            self.vendor_ids.append(vendor_id)
+            cursor.execute(
+                """INSERT INTO DiscoverVendorCategory (vendor_id, category_id) VALUES (%s, %s)""", 
+                (vendor_id, self.vendor_categories[0])
+            )
+            
+    def insert_post_1(self, cursor):
+        """Inserts a post from Elon Gates, tags Salesforce, comments from Bill and Mary, Elon likes Bill's comment"""
+        # Insert post
+        cursor.execute(
+            """INSERT INTO Post (user_id, is_anonymous, title, body) VALUES (%s, %s, %s, %s)""",
+            (self.test_user_ids[0], False, "Salesforce is awesome", "I've sold so many cars.")
+        )
+        cursor.execute("SELECT LAST_INSERT_ID()")
+        post_id = cursor.fetchone()['LAST_INSERT_ID()']
 
-    #             # Link post to vendor
-    #             cursor.execute("INSERT INTO PostVendor (post_id, vendor_id) VALUES (%s, %s)", (post_id, vendor_id))
+        # Tag Salesforce in post
+        cursor.execute(
+            """INSERT INTO PostVendor (post_id, vendor_id) VALUES (%s,%s)""",
+            (post_id, self.vendor_ids[0])
+        )
+        # Insert comment from Bill
+        cursor.execute(
+            """INSERT INTO Comment (post_id, user_id, comment_text) VALUES (%s, %s, %s)""",
+            (post_id, self.test_user_ids[1], "Salesforce is a great tool!")
+        )
+        cursor.execute("SELECT LAST_INSERT_ID()")
+        comment_id = cursor.fetchone()['LAST_INSERT_ID()']
 
-    #             postNum = postNum + 1
+        # Insert comment from Mary
+        cursor.execute(
+            """INSERT INTO Comment (post_id, user_id, comment_text) VALUES (%s, %s, %s)""",
+            (post_id, self.test_user_ids[2], "Maybe I should try it so I can sell more cars too")
+        )
 
-    # # Insert 20 mock comments into the first post
-    # commentPost = post_ids[0]
-    # for i in range(20):
-    #     cursor.execute("INSERT INTO Comment (post_id, user_id, comment_text) VALUES (%s, %s, 'Mock Comment %s')", (commentPost, user_id, i + 1))
-
-    return False
+        # Elon likes Bill's comment
+        cursor.execute(
+            """INSERT INTO CommentUpvote (comment_id, user_id) VALUES (%s, %s)""",
+            (comment_id, self.test_user_ids[0])
+        )
 
 if __name__ == '__main__':
     # Establish database conn
