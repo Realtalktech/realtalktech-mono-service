e5075b95c8feeac562c958553f9412720e9bfe3e
raghavProactifi <raghav@proactifi.com>
2024-02-11 12:45:43 -0500

    login to take json argument

--- routes/login.py
+++ routes/login.py
@@ -111,8 +111,9 @@ def signup():
 @login_bp.route('/login', methods=['POST'])
 def login():
     # Extract username and password from the request
-    username = request.form.get('username')
-    password = request.form.get('password')
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
 
     if not username or not password:
         return jsonify({"error": "Username and password are required"}), 400
