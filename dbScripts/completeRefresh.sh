97589be2c65805b3f52802d7e2374a4026e75da4
raghavProactifi <raghav@proactifi.com>
2024-02-12 12:56:16 -0500

    changes to git ignore

--- dbScripts/completeRefresh.sh
+++ dbScripts/completeRefresh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source dbScripts/
 source dbScripts/teardownDb.sql
 source dbScripts/initDb.sql
 # source dbScripts/verify_tables_exist.sql
--- dbScripts/connect/unsafeConnect.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Global variables
-RDS_ENDPOINT="realtalktechrdsstack-realtalktechdbinstance-c7ciisdczocf.cnqm62ueodz0.us-east-1.rds.amazonaws.com"
-USERNAME="admin"
-PASSWORD="ReallyRealAboutTech123!"
-
-# Connect to the RDS instance
-mysql -h $RDS_ENDPOINT -u $USERNAME -p$PASSWORD
