b639befb80ca866b0170dc7f349ca56b4cdd8f6a
raghavProactifi <raghav@proactifi.com>
2024-03-06 21:53:21 -0500

    fixes

--- rtt_data_app/utils/__init__.py
+++ rtt_data_app/utils/__init__.py
@@ -1,5 +1,5 @@
 # utils/__init__.py
-from .db_manager import DBManager
+# from .db_manager import DBManager
 from .log_config import setup_global_logging
 from .post import Post
 from .user import User
