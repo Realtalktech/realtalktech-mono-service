ad6aab5154f2fbcfc8f689efe31e3e0b385c0e68
raghavProactifi <raghav@proactifi.com>
2024-03-23 19:21:15 -0400

    Added SSL connection details

--- rtt_data_app/config.py
+++ rtt_data_app/config.py
@@ -9,6 +9,12 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     DEBUG = True
     LOG_PATH = '/logs/flask_app'
+    SSL_CONFIG = {
+        'ssl': {
+        'ca': '/config/global-bundle.pem'  # Path to the downloaded AWS RDS certificate
+        }
+    }
+    SQLALCHEMY_ENGINE_OPTIONS = {'connect_args':SSL_CONFIG}
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -18,8 +24,5 @@ class DevelopmentConfig(Config):
     DB_NAME = 'dev_db'
 
 class TestingConfig(Config):
-    # file_path = os.path.abspath()
     SQLALCHEMY_DATABASE_URI = 'sqlite:////Users/raghavmaini/Desktop/rttMVP/rttDataService/tests/sqlite/test_database.db'
     LOG_PATH = 'tests/logs'
-
-
